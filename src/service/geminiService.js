21917dbd535dbd13cfef4f58f430d3906b704dd7
Phelps <ultimaterafa79@gmail.com>
2024-12-05 19:20:14 -0300

    algumas modificações na paste serice, responsável pela dependência do Google Gemini.

--- src/service/geminiService.js
+++ src/service/geminiService.js
@@ -4,7 +4,7 @@ const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
 export default async function gerarDescricaoComGemini(imageBuffer) {
-    const prompt = "Gere uma descrição em português do Brasil para a seguinte imagem";
+    const prompt = "Gere uma descrição em português do Brasil de no máximo sessenta palavras para a seguinte imagem, mas sem dizer 'Aqui está uma descrição para a imagem...'.";
 
     try {
         const image = {
